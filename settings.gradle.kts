1684da52cb2a3f602a2503ef554aed78468c608c
Christos Hadjinikolis <christos.hadjinikolis@vortexa.com>
2026-07-07 15:48:11 +0100

    fix: simplify Gradle settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,1 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        mavenCentral()
-    }
-}
-
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        mavenCentral()
-        intellijPlatform {
-            defaultRepositories()
-        }
-    }
-}
-
 rootProject.name = "skeleton-replay-plugin"
